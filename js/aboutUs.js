0dfd596c855b917adfb7340528ad7653d20da237
VinamraBhavsar <vinamra842000@gmail.com>
2024-04-13 15:11:56 -0400

    Add JQuery Box Slider For Review

---
+++ js/aboutUs.js
@@ -0,0 +1,11 @@
+$(document).ready(() =>
+    $("#slider").bxSlider({
+        auto: true,
+        autoControls: true,
+        captions: false,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 550,
+        slideMargin: 10
+    })
+)
